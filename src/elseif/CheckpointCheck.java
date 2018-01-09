63894bcb3116781add1879fe53b973c5353acf78
League <league@WTS8.attlocal.net>
2018-01-08 18:24:20 -0800

    RobotColorChooser

---
+++ src/elseif/CheckpointCheck.java
@@ -0,0 +1,12 @@
+package elseif;
+
+import javax.swing.JOptionPane;
+
+public class CheckpointCheck {
+public static void main(String[] args) {
+	String d =JOptionPane.showInputDialog("How many cups of flour do you have?");
+			
+		JOptionPane.showMessageDialog(null, "go to the store and buy more");
+	}
+}
+
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -9,24 +9,38 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-
+		Robot bot= new Robot();
 		//3. ask the user what color they would like the robot to draw
-		
+		String answer = JOptionPane.showInputDialog("What color would you like to draw");
 		//4. use an if/else statement to set the pen color that the user requested
+		if(answer.equals("red")) {
+			bot.setPenColor(255, 0, 0);
+		
+		
+		}	
+		else if(answer.equals("green")) {
+			bot.setPenColor(0, 255, 0);
+		
+		
+		
+		
+		}
 
         //5. if the user doesn’t enter anything, choose a random color
-
+				bot.setPenColor(0,0,255);
+				
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//2. set the pen width to 10
-		
+		bot.setPenWidth(10);
 	    //1. make the robot draw a shape (this will take more than one line of code)
-			String color= JOptionPane.showInputDialog("What color would you like to draw");
-		Robot bot= new Robot();
-		size(10,10);
+			
+
+		
 					
 					bot.setSpeed(20);
 					bot.penDown();
@@ -37,15 +51,8 @@ public class RobotColorChooser {
 					bot.move(100);
 					bot.turn(90);
 					bot.move(100);
-					bot.setPenWidth(10);
-				if(bot.equals(color));
-				bot.setPenColor(0, 0, 0);
+					
+				
 				
 	}			
-
-
-	private static void size(int i, int j) {
-		// TODO Auto-generated method stub
-		
-	}
 }
