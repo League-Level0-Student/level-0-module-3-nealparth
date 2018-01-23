701924e0bee3826f4ecc994ca6d0ed29e8d492c9
Kadorus <League@AdministorsiMac.attlocal.net>
2018-01-22 18:26:04 -0800

    HighLowGame,Birthday Reminder

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,21 +11,35 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "May 9th";
+		String dadsBirthday = "December 15";
+		String myBirthday = "October 10";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+			String Birthday= JOptionPane.showInputDialog(null, "Which birthday do you want?");
 		// 3. Print out what the user typed
-		
+		System.out.println(Birthday);
 		// 4. if user asked for "mom"
+		if(Birthday.equals("mom")) {
+			System.out.println(momsBirthday);
+		}
+		
+			
 			//print mom's birthday
 		// 5. if user asked for "dad"
+		else if(Birthday.equals("dad")) {
+			System.out.println(dadsBirthday);
+		}
 			// print dad's birthday
 		// 6. if user asked for your name
+		else if(Birthday.equals("neal the seal")) {
+			System.out.println(myBirthday);
+		}
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		else {
+		JOptionPane.showMessageDialog(null,"Sorry, i don't remember that person's birthday!");
 	} 
+	
+}
 }
--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,29 +5,43 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(100);
 		// 2. Print out the random variable above
-		
+		System.out.print(random);
 		// 11. do the following 10 times
-		
-			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+		for (int i = 1; i < 11; i++) {
+			// 1. ask the user for a guess using a pop-up window, and save their response
+			String answer = JOptionPane.showInputDialog("take a guess?");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int d = Integer.parseInt(answer);
 			// 5. if the guess is correct
-				// 6. win
+			if (d == random) {
+				JOptionPane.showMessageDialog(null, "You are the lucky winner");
+			}
+			// 6. win
 			// 7. if the guess is high
-				// 8. tell them it's too high
+			if (d > random) {
+				JOptionPane.showMessageDialog(null, "Too High Bruh");
+			}
+			// 8. tell them it's too high
 			// 9. if the guess is low
-				// 10. tell them it's too low
+			if (d < random) {
+				JOptionPane.showMessageDialog(null, "Too Low Bruh");
+			}
 
-		// 12. tell them they lose
+			// 10. tell them it's too low
+
+			// 12. tell them they lose
+
+		}
+		JOptionPane.showMessageDialog(null, "Bruh you lost");
+
 	}
 
 }
-
-
